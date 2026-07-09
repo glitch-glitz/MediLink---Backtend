45971446ab0fb2f74db0c6e504fc0435b60e7f2b
Helix <ajangome@yahoo.com>
2026-07-09 20:02:00 +0300

    Create User model and database migration

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,6 @@
 from app.models.product import Product
 from app.models.contact import Contact
 from app.models.user import User
+from .product import Product
+from .contact import Contact
+from .user import User
--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
 
 from app.database import Base
 
@@ -8,10 +8,10 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
 
-    username = Column(String(100), unique=True, nullable=False)
+    name = Column(String(150), nullable=False)
 
-    email = Column(String(255), unique=True, nullable=False)
+    email = Column(String(255), unique=True, index=True, nullable=False)
 
     hashed_password = Column(String(255), nullable=False)
 
-    is_admin = Column(Boolean, default=True)
+    is_admin = Column(Boolean, default=False)
